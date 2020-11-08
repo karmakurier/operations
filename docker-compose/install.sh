fbcd77c9042592eec13d0883b318e57e03f9193c
Modding <valentin@seipt.org>
2020-11-08 18:43:29 +0100

    Addet Installer for lazy as Frick Admins!

---
+++ docker-compose/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt update
+apt -y upgrade
+apt -y install git docker-compose
+git clone https://github.com/karmakurier/karmakurier-frontend.git
+git clone https://github.com/karmakurier/api.git
+mkdir -p db/dbdata
+echo "SETUP COMPLETE! You may now edit the Configuration of the .env file and edit the Webserver configuration of the Frontend Container."
