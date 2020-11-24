45347ca496ef0ca8b452e22353b0b5857e8530cc
Modding <valentin@seipt.org>
2020-11-24 16:00:57 +0100

    contact backend in install addet

--- docker-compose/install.sh
+++ docker-compose/install.sh
@@ -5,5 +5,6 @@ apt -y upgrade
 apt -y install git docker-compose
 git clone https://github.com/karmakurier/karmakurier-frontend.git
 git clone https://github.com/karmakurier/api.git
+git clone https://github.com/karmakurier/contact-backend.git
 mkdir -p db/dbdata
 echo "SETUP COMPLETE! You may now edit the Configuration of the .env file and edit the Webserver configuration of the Frontend Container."
